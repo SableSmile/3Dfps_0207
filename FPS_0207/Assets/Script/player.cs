584a41bdae37e860f9363a41f588add4dcb74515
SableSmile <32065905+SableSmile@users.noreply.github.com>
2021-02-07 17:14:48 +0800

    0207 腳色移動

---
+++ FPS_0207/Assets/Script/player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //移動速度
+    public float speed;
+    public float turn;
+
+    private Animator ani;
+    private Rigidbody rb;
+
+    private void Awake() {
+        ani=GetComponent<Animator>();
+        rb=GetComponent<Rigidbody>();
+    }
+    private void Update() {
+        Move();
+    }
+    public void Move(){
+        float v=Input.GetAxis("Vertical");
+        float h=Input.GetAxis("Horizontal");
+        rb.MovePosition(transform.position+transform.forward*v*speed * Time.deltaTime+transform.right*h*speed*Time.deltaTime);
+    }
+}
